daeb20be2998fea1fd799baf33caef89d2e049fb
Eugene Cheltsov <chill.icp@gmail.com>
2023-10-23 19:53:27 +0900

    initial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Clock App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const HomePage());
+  }
+}
